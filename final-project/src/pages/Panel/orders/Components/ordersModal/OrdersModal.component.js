04eac6ecfdf50fccf1dd5ec0a5ed8fd1f57bd8ec
mohammadhnafar <darklord8100@gmail.com>
2022-03-03 17:58:33 +0330

    orders modal done

--- final-project/src/pages/Panel/orders/Components/ordersModal/OrdersModal.component.js
+++ final-project/src/pages/Panel/orders/Components/ordersModal/OrdersModal.component.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Styles from './ordersModal.module.css';
+import {connect} from 'react-redux';
+import {useDispatch, useSelector } from 'react-redux';
 
 const OrdersmodalComponent = (props) => {
+   
+    const modalData = useSelector(state => state.orders.modal)
+    console.log(modalData)
     return (
         <div className={Styles.container}>
         
@@ -18,12 +23,15 @@ const OrdersmodalComponent = (props) => {
                 </div>
                 <div className={Styles.items}>
                     <div className={Styles.item}>
-                        <h3>{""} زمان ثبت سفارش : </h3> 
-                        <h3>{""} شماره تماس : </h3> 
-                        <h3>{""} نشانی : </h3> 
-                        <h3>{""} مجموع کالا ها : </h3> 
-                        <h3>{""} مبلغ کل : </h3>
-                        <h3>{""} وضعیت تحویل : </h3>
+                          <h3> نام کاربر : {modalData.name}  </h3> 
+                          <h3> کالا ها  : {modalData.products}  </h3> 
+                        <h3>{""} زمان ثبت سفارش : {modalData.date} </h3> 
+                        <h3>{""} شماره تماس :{modalData.phone}  </h3> 
+                        <h3>{""} ایمیل : {modalData.email} </h3> 
+                        <h3>{""} نشانی : {modalData.address} </h3> 
+                        <h3>{""} مجموع کالا ها : {modalData.totalItems} </h3> 
+                        <h3>{""} مبلغ کل : {modalData.totalPrice} </h3>
+                        <h3>{""} وضعیت تحویل : {modalData.status} </h3>
                         </div>
                     
                     
@@ -34,4 +42,5 @@ const OrdersmodalComponent = (props) => {
         </div>
     );
     }
+    
 export default OrdersmodalComponent;
