deb52de1c8398c03a9eca7c5410c0d845fcab719
mohammadhnafar <darklord8100@gmail.com>
2022-03-04 23:37:37 +0330

    add products for showing products by categorys

---
+++ final-project/src/pages/Products/Products.page.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Styles from './products.module.css'
+import Header from '../../layouts/user/header/Header'
+import {useEffect, useState, useRef} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../../redux/Shopping/shopping.thunk';
+import Card from '../../Components/Cards/Card.component'
+
+import { useParams } from 'react-router';
+
+const ProductsPage = () => {
+    const productsNew = useSelector(state => state.shop.products.data)
+    const dispatch = useDispatch();
+    let {category} = useParams(); 
+
+    console.log(category)
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, [])
+      
+
+
+
+
+
+
+    return (
+        <div>
+            <Header/>
+            <h1>this is products</h1>
+            <h1>{category}</h1>
+               { <div className={Styles.firstSecCards}>
+                { productsNew?.filter(value=> value.category == category).map(
+                 values => 
+                    <Card
+                                id = {values.id}
+                                key = {values.id}
+                    Name = {values.name}
+                    info = {values.category}
+                    Price = {values.price}
+                    PicList = {values.image}
+                    count = {values.count}
+                    
+                    />
+            )
+} 
+                </div> }
+            
+        </div>
+    );
+}
+
+export default ProductsPage;
