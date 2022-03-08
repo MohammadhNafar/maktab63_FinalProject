5cb110180bf49882e109f8c19c5a4dd84848bc9c
mohammadhnafar <darklord8100@gmail.com>
2022-03-08 09:32:42 +0330

    little changes for cleaning codes

--- final-project/src/pages/Panel/quantity/Quantity.page.js
+++ final-project/src/pages/Panel/quantity/Quantity.page.js
@@ -7,91 +7,92 @@ import {getProducts} from '../../../api/products.api';
 import Pagination from '../../../Components/pagination/pagination.component';
 
 const QuantityPage = () => {
+const [rows, setRows] = useState([]);
+const [filteredData, setFilter] = useState([])
+const [currentPage, setCurrentPage] = useState(1);
+const [postsPerPage] = useState(5);
 
-    const [rows, setRows] = useState([]);
-    const [filteredData, setFilter] = useState([])
-    const [currentPage , setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(5);
+function handleClick(e) {
+    e.preventDefault();
+    getProducts().then(data => setFilter(data.data))
+    setFilter(rows.filter(item => item.count > 0))
 
-    function handleClick(e) {
-        e.preventDefault();
-        getProducts().then(data => setFilter(data.data))
-        setFilter(rows.filter(item => item.count > 0))
+}
 
-    }
+useEffect(() => {
+    getProducts().then(data => setRows(data.data))
+   
+}, [])
 
-    useEffect(() => {
-        getProducts().then(data => setRows(data.data))
-        console.log(rows, 'hello')
-    }, [])
-    
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = rows.slice(indexOfFirstPost, indexOfLastPost)
-    const filterdPaginate = filteredData.slice(indexOfFirstPost, indexOfLastPost);
-      //change page
-      const paginate = (pageNumber) => setCurrentPage(pageNumber);
+const indexOfLastPost = currentPage * postsPerPage;
+const indexOfFirstPost = indexOfLastPost - postsPerPage;
+const currentPosts = rows.slice(indexOfFirstPost, indexOfLastPost)
+const filterdPaginate = filteredData.slice(indexOfFirstPost, indexOfLastPost);
+//change page
+const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    return (
+return (
 
-        <div>
-            <Header/>
-            <div className={Styles.buttons}>
-                <button onClick={(e) =>{
+    <div>
+        <Header/>
+        <div className={Styles.buttons}>
+            <button
+                onClick={(e) => {
 
-                  e.preventDefault();
-                  
-                  setFilter(rows.filter(item => item.count > 0))
-                  
+                    e.preventDefault();
+
+                    setFilter(rows.filter(item => item.count > 0))
 
                 }}>
-                    نمایش کالا های موجود
-                </button>
-                <button onClick={(e) =>{
-                  window.location.reload();
-                }} >نمایش همه
-                </button>
+                نمایش کالا های موجود
+            </button>
+            <button
+                onClick={(e) => {
+                    window
+                        .location
+                        .reload();
+                }}>نمایش همه
+            </button>
+        </div>
+        <div className={Styles.wrapper}>
+            <div className={Styles.title}>
+                <h1>موجودی ها</h1>
             </div>
-            <div className={Styles.wrapper}>
-                <div className={Styles.title}>
-                    <h1>موجودی ها</h1>
-                </div>
-                <div className={Styles.tableHead}>
-                    <h1>موجودی</h1>
-                    <h1>قیمت</h1>
-                    <h1>نام کالا
-                    </h1>
+            <div className={Styles.tableHead}>
+                <h1>موجودی</h1>
+                <h1>قیمت</h1>
+                <h1>نام کالا
+                </h1>
 
-                </div>
-                <div className={Styles.quantityList}>
-                    {
-                        filterdPaginate.length > 0
-                            ? filterdPaginate.map(
-                                data => <Table
-                                    key={data.id}
-                                    nameList={data.name}
-                                    priceList={`${data.price} تومان`}
-                                    quanitityList={data.count}/>
+            </div>
+            <div className={Styles.quantityList}>
+                {
+                    filterdPaginate.length > 0
+                        ? filterdPaginate.map(
+                            data => <Table
+                                key={data.id}
+                                nameList={data.name}
+                                priceList={`${data.price} تومان`}
+                                quanitityList={data.count}/>
 
-                            )
-                            : currentPosts.map(
-                                data => <Table
-                                    key={data.id}
-                                    nameList={data.name}
-                                    priceList={`${data.price} تومان`}
-                                    quanitityList={data.count}/>
+                        )
+                        : currentPosts.map(
+                            data => <Table
+                                key={data.id}
+                                nameList={data.name}
+                                priceList={`${data.price} تومان`}
+                                quanitityList={data.count}/>
 
-                            )
-                    }
-                    <Pagination
-             postsPerPage={postsPerPage}
-             totalPosts = {rows.length}
-             paginate = {paginate}
-             />
-                </div>
+                        )
+                }
+                <Pagination
+                    postsPerPage={postsPerPage}
+                    totalPosts={rows.length}
+                    paginate={paginate}/>
             </div>
         </div>
-    );
+    </div>
+);
 }
 
 export default QuantityPage;
